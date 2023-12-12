0cf1ee22e54541b434fffa90d62d91354828c32c
ariel.duarte <arielpy@gmail.com>
2023-12-12 18:57:11 -0300

    Implemented ShellRoute as example to navigate on menu botton navbar

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,23 +1,57 @@
 import 'package:go_router/go_router.dart';
+import 'package:moviepedia_app/presentation/views/views.dart';
 
 import '../../presentation/screens/screens.dart';
 
-final appRouter = GoRouter(
-  routes: [
-    GoRoute(
-        path: '/',
-        name: HomeScreen.name,
-        builder: (context, state) => const HomeScreen(),
-        routes: [
-          GoRoute(
-              path: 'movie/:id',
-              name: MovieScreen.name,
-              builder: (context, state) {
-                final idParam = state.pathParameters;
-                return MovieScreen(
-                  movieId: idParam['id'] ?? 'no-id',
-                );
-              }),
-        ]),
-  ],
-);
+final appRouter = GoRouter(initialLocation: '/', routes: [
+  ShellRoute(
+      builder: (context, state, child) {
+        return HomeScreen(
+          childView: child,
+        );
+      },
+      routes: [
+        GoRoute(
+            path: '/',
+            builder: (context, state) {
+              return const HomeView();
+            },
+            routes: [
+              GoRoute(
+                  path: 'movie/:id',
+                  name: MovieScreen.name,
+                  builder: (context, state) {
+                    final idParam = state.pathParameters;
+                    return MovieScreen(
+                      movieId: idParam['id'] ?? 'no-id',
+                    );
+                  }),
+            ]),
+        GoRoute(
+          path: '/favorites',
+          builder: (context, state) {
+            return const FavoritesView();
+          },
+        ),
+      ]
+
+      /** Configuracion simple rutas padre/hijo */
+      // GoRoute(
+      //     path: '/',
+      //     name: HomeScreen.name,
+      //     builder: (context, state) => const HomeScreen(
+      //           childView: FavoritesView(),
+      //         ),
+      //     routes: [
+      //       GoRoute(
+      //           path: 'movie/:id',
+      //           name: MovieScreen.name,
+      //           builder: (context, state) {
+      //             final idParam = state.pathParameters;
+      //             return MovieScreen(
+      //               movieId: idParam['id'] ?? 'no-id',
+      //             );
+      //           }),
+      //     ]),
+      )
+]);
--- lib/presentation/screens/widgets/shared/custom_bottom_navbar.dart
+++ lib/presentation/screens/widgets/shared/custom_bottom_navbar.dart
@@ -1,16 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomBottomNavbar extends StatelessWidget {
   const CustomBottomNavbar({super.key});
 
+  int getCurrentIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    switch (location) {
+      case '/':
+        return 0;
+      case '/categories':
+        return 1;
+      case '/favorites':
+        return 2;
+      default:
+        return 0;
+    }
+  }
+
+  void onItemTapped(BuildContext context, int index) {
+    switch (index) {
+      case 0:
+        context.go('/');
+        break;
+      case 1:
+        context.go('/');
+        break;
+      case 2:
+        context.go('/favorites');
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(elevation: 0, items: const [
-      BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.label_outline), label: 'Categories'),
-      BottomNavigationBarItem(
-          icon: Icon(Icons.favorite_border_outlined), label: 'Favorites'),
-    ]);
+    return BottomNavigationBar(
+        onTap: (value) => onItemTapped(context, value),
+        currentIndex: getCurrentIndex(context),
+        elevation: 0,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.label_outline), label: 'Categories'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite_border_outlined), label: 'Favorites'),
+        ]);
   }
 }
