3853f1de1064cd7ff565114555bdc8cf3f15c7ab
ariel.duarte <arielpy@gmail.com>
2023-12-16 13:43:22 -0300

    Implemented favorite movies provider to show a list movies on Favorite view

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:moviepedia_app/presentation/providers/storage/favorite_movies_provider.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites'),
-      ),
-      body: const Center(
-        child: Text('Favorites'),
-      ),
-    );
+        body: ListView.builder(
+      itemCount: favoriteMovies.length,
+      itemBuilder: (context, index) {
+        final movie = favoriteMovies[index];
+        return ListTile(
+          title: Text(movie.title),
+        );
+      },
+    ));
   }
 }
