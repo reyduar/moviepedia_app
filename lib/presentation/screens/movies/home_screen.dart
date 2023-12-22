a1a3bfe72e247dd50da4af2f4ee844e682e54207
ariel.duarte <arielpy@gmail.com>
2023-12-21 22:16:26 -0300

    Implemented AutomaticKeepAliveClientMixin to keep page view state on home screen

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -2,21 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:moviepedia_app/presentation/widgets/widgets.dart';
 import '../../views/views.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const name = 'home-screen';
   final int pageIndex;
   const HomeScreen({super.key, required this.pageIndex});
 
-  final viewRoutes = const <Widget>[HomeView(), PopularView(), FavoritesView()];
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+//* Este Mixin es necesario para mantener el estado en el PageView
+class _HomeScreenState extends State<HomeScreen>
+    with AutomaticKeepAliveClientMixin {
+  late PageController pageController;
+
+  @override
+  void initState() {
+    super.initState();
+    pageController = PageController(keepPage: true);
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  final viewRoutes = const <Widget>[
+    HomeView(),
+    PopularView(), // <--- categorias View
+    FavoritesView(),
+  ];
 
   @override
   Widget build(BuildContext context) {
+    super.build(context);
+
+    if (pageController.hasClients) {
+      pageController.animateToPage(
+        widget.pageIndex,
+        curve: Curves.easeInOut,
+        duration: const Duration(milliseconds: 250),
+      );
+    }
+
     return Scaffold(
-      body: IndexedStack(
-        index: pageIndex,
+      body: PageView(
+        //* Esto evitará que rebote
+        physics: const NeverScrollableScrollPhysics(),
+        controller: pageController,
+        // index: pageIndex,
         children: viewRoutes,
       ),
-      bottomNavigationBar: CustomBottomNavbar(currentIndex: pageIndex),
+      bottomNavigationBar: CustomBottomNavbar(
+        currentIndex: widget.pageIndex,
+      ),
     );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
